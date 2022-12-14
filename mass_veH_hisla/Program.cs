f3a857cfa2ee7efcbe61a53ef2b0fba70cf828b9
Andrei <dasd_78@mail.ru>
2022-12-14 19:44:08 +0300

    массив вещественных чисел

---
+++ mass_veH_hisla/Program.cs
@@ -0,0 +1,12 @@
+﻿double[,] a = new double[5, 10];
+
+Random random = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        a[i, j] = random.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
